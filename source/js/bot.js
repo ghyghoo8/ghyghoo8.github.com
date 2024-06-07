396128074911c95a3ec01e7fe435d825a27c0daf
ghyghoo8 <ghyghoo8@gmail.com>
2024-06-07 18:04:01 +0800

    feat: save

--- source/js/bot.js
+++ source/js/bot.js
@@ -1,4 +1,12 @@
-window.embeddedChatbotConfig = {
-  chatbotId: "z322nJiXx5sA8wPuwGyAM",
-  domain: "www.chatbase.co"
-}
+(function () {
+  window.embeddedChatbotConfig = {
+    chatbotId: "z322nJiXx5sA8wPuwGyAM",
+    domain: "www.chatbase.co"
+  }
+    
+  let script = document.createElement('script');
+  script.src = 'https://www.chatbase.co/embed.min.js';
+  script.setAttribute('chatbotId', 'z322nJiXx5sA8wPuwGyAM')
+  script.setAttribute('domain', 'www.chatbase.co')
+  document.getElementsByTagName('body')[0].appendChild(script);
+})()
--- themes/next/scripts/chatbot.js
+++ themes/next/scripts/chatbot.js
@@ -1,5 +1,5 @@
 const js = hexo.extend.helper.get('js').bind(hexo);
 
 hexo.extend.injector.register('head_end', () => {
-  return js('/js/chatbase.js');
+  return js('/js/bot.js');
 });
