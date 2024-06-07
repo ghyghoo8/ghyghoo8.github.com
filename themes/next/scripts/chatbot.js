986923660962d712cd40240582a20a7f092bb873
ghyghoo8 <ghyghoo8@gmail.com>
2024-06-07 17:59:26 +0800

    feat: add injector js

--- themes/next/scripts/chatbot.js
+++ themes/next/scripts/chatbot.js
@@ -1,4 +1,5 @@
-hexo.extend.filter.register('theme_inject', function(injects) {
-  //名字路径等都可以随意修改，为了方便下文都以这里的定义为主
-  injects.head.file('script', '/js/chatbase.js', {}, {cache: true});
+const js = hexo.extend.helper.get('js').bind(hexo);
+
+hexo.extend.injector.register('head_end', () => {
+  return js('/js/chatbase.js');
 });
