bc5b499dd745efeb143d59769762df27893fee81
ghyghoo8 <ghyghoo8@gmail.com>
2024-06-07 18:52:09 +0800

    feat: fix

--- source/js/bot.js
+++ source/js/bot.js
@@ -4,11 +4,9 @@
     domain: "www.chatbase.co"
   }
     
-  window.onload = function () {
-    let script = document.createElement('script');
-    script.src = 'https://www.chatbase.co/embed.min.js';
-    script.setAttribute('chatbotId', 'z322nJiXx5sA8wPuwGyAM')
-    script.setAttribute('domain', 'www.chatbase.co')
-    document.body.appendChild(script);
-  }
+  let script = document.createElement('script');
+  script.src = 'https://www.chatbase.co/embed.min.js';
+  script.setAttribute('chatbotId', 'z322nJiXx5sA8wPuwGyAM')
+  script.setAttribute('domain', 'www.chatbase.co')
+  document.body.appendChild(script);
 })()
