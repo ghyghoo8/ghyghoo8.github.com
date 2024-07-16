c68211bf11a024aefced8507f8c1f371c5b9ad5a
ghyghoo8 <ghyghoo8@gmail.com>
2024-07-16 14:30:08 +0800

    feat: add google adsense

--- scripts/injector.js
+++ scripts/injector.js
@@ -3,3 +3,8 @@ const js = hexo.extend.helper.get('js').bind(hexo);
 hexo.extend.injector.register('body_end', () => {
   return js('/js/bot.js');
 });
+
+// google adsense
+hexo.extend.injector.register('head_end', () => {
+  return '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7923526446101866" crossorigin="anonymous"></script>'
+});
