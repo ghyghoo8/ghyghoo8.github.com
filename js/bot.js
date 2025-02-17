abed2e1e583c1e15350dc099be08d509ff368870
ghyghoo8 <ghyghoo8@qq.com>
2025-02-17 09:22:58 +0800

    Site updated: 2025-02-17 09:22:58

---
+++ js/bot.js
@@ -0,0 +1,10 @@
+(function () {
+  window.difyChatbotConfig = {
+    token: 'C5e3qHCcAulMHOuD'
+  }
+  
+  let script = document.createElement('script');
+  script.src = 'https://udify.app/embed.min.js';
+  script.setAttribute('id', 'C5e3qHCcAulMHOuD')
+  document.body.appendChild(script);
+})()
